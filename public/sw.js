cf660038ada796dd531233ea7838d928d891f301
fborlido <fborlido2@gmail.com>
2020-11-10 00:10:04 +0000

    gyugu

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,7 @@
 var CACHE_NAME = 'fred-shop';
 
 var urlsToCache = [
-    '/',
-    '/static/js/bundle.js',
-    '/static/js/main.chunk.js',
-    '/static/js/1.chunk.js',
-    '/static/js/0.chunk.js',
-    '/favicon.ico',
-    '/css?family=Open+Sans',
-    '/icon?family=Material+Icons'
+    '/'
 ];
 
 self.addEventListener('install', function (event) {
